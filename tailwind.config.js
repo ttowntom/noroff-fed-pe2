ee04a8489f879e2a9529afc71eaa917f64aac31d
ttowntom <git.running054@passinbox.com>
2025-01-13 13:59:17 +0100

    installs fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{html,js,jsx}"],
-	darkMode: "class",
-	theme: {
-		extend: {
-			colors: {
-				info: "#14213D",
-				warn: "#FCA311",
-				success: "#4B8175",
-				"success-light": "#DCEBE6",
-			},
-			fontFamily: {
-				roboto: ["Roboto", "sans-serif"],
-			},
-		},
-		screens: {
-			xsm: "350px",
-			sm: "640px",
-			md: "768px",
-			lg: "1024px",
-			xl: "1280px",
-			"2xl": "1536px",
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{html,js,jsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        info: "#14213D",
+        warn: "#FCA311",
+        success: "#4B8175",
+        "success-light": "#DCEBE6",
+      },
+      fontFamily: {
+        noto: ["Noto Sans", serif],
+        notoSerif: ["Noto Serif", serif],
+      },
+    },
+    screens: {
+      xsm: "350px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  plugins: [],
 };
