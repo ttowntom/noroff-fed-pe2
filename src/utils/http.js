37c05b2c3d33bf4867de7f83d9c05c9a5a24aa6f
ttowntom <git.running054@passinbox.com>
2025-01-14 14:24:19 +0100

    adds tanstack http functions

---
+++ src/utils/http.js
@@ -0,0 +1,100 @@
+import { QueryClient } from "@tanstack/react-query";
+
+import { API_HOLIDAZE_URL, API_KEY } from "../constants";
+
+const baseUrl = `${API_HOLIDAZE_URL}`;
+
+export const queryClient = new QueryClient();
+
+export async function fetchFn({ queryKey }) {
+  const [url, token] = queryKey;
+
+  try {
+    const res = await fetch(`${baseUrl}${url}`, {
+      headers: {
+        "Content-Type": "application/json",
+        "X-Noroff-API-Key": API_KEY,
+      },
+    });
+    const data = await res.json();
+
+    if (!response.ok) {
+      throw new Error(data.error || "An error occurred while fetching data");
+    }
+
+    return data;
+  } catch (error) {
+    throw new Error(error.message || "Failed to make request");
+  }
+}
+
+export async function postFn({ url, body, token }) {
+  try {
+    const response = await fetch(`${baseUrl}${url}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+        "X-Noroff-API-Key": API_KEY,
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.error || "An error occurred while posting data");
+    }
+
+    return data;
+  } catch (error) {
+    throw new Error(error.message || "Failed to make request");
+  }
+}
+
+export async function putFn({ url, body, token }) {
+  try {
+    const response = await fetch(`${baseUrl}${url}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+        "X-Noroff-API-Key": API_KEY,
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.error || "An error occurred while updating data");
+    }
+
+    return data;
+  } catch (error) {
+    throw new Error(error.message || "Failed to make request");
+  }
+}
+
+export async function deleteFn({ url, token }) {
+  try {
+    const response = await fetch(`${baseUrl}${url}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+        "X-Noroff-API-Key": API_KEY,
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.error || "An error occurred while deleting data");
+    }
+
+    return data;
+  } catch (error) {
+    throw new Error(error.message || "Failed to make request");
+  }
+}
