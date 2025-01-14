e5cf92ed660fb4b69a4e15b7d0ac7e2543131b27
ttowntom <git.running054@passinbox.com>
2025-01-14 15:42:46 +0100

    creates temporary components

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,18 +1,16 @@
 import { Outlet } from "react-router-dom";
 
-// import Navbar from "../components/Navbar";
-// import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function RootLayout() {
   return (
     <div className="flex min-h-svh flex-col">
-      {/* <Navbar /> */}
-      <p>Nav</p>
+      <Navbar />
       <main className="mx-auto flex w-full max-w-[1536px] flex-grow flex-col px-4">
         <Outlet />
       </main>
-      {/* <Footer /> */}
-      <p>Footer</p>
+      <Footer />
     </div>
   );
 }
