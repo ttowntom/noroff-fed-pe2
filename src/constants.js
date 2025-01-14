a6bcfa6f750bbebec3372c5f8bbef08e616125c5
ttowntom <git.running054@passinbox.com>
2025-01-14 13:30:36 +0100

    adds constants

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const API_HOST_URL = ".api.noroff.dev";
+export const API_VERSION = "v2";
+export const API_BASE_URL = `https://${API_VERSION}${API_HOST_URL}`;
+export const API_HOLIDAZE_BASE = "/holidaze";
+export const API_HOLIDAZE_URL = `https://${API_VERSION}${API_HOST_URL}${API_AUCTION_BASE}`;
+
+export const NO_VENUE_IMG_URL =
+  "https://images.pexels.com/photos/28216688/pexels-photo-28216688/free-photo-of-autumn-camping.png";
+export const NO_USER_IMG_URL =
+  "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg";
