c431211ac7ee98248eb7889e8ec26f6ad38a401b
ttowntom <git.running054@passinbox.com>
2025-01-15 09:57:27 +0100

    adds temp pages to test router

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,25 @@ import { queryClient } from "./utils/http";
 import "./App.css";
 import RootLayout from "./pages/RootLayout";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Venues from "./pages/Venues";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout />,
-      children: [{ index: true, element: <Signup /> }],
+      children: [
+        { index: true, element: <Venues /> },
+        { path: "signup", element: <Signup /> },
+        { path: "login", element: <Login /> },
+      ],
     },
   ]);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider
-        future={{
-          v7_startTransition: true,
-        }}
-        router={router}
-      />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 }
