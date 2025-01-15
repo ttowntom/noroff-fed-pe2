024935f11062afab1c0ab3fb87e6a6019284699f
ttowntom <git.running054@passinbox.com>
2025-01-15 17:22:57 +0100

    adds hover effect to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ export default function Button({ onClick, children, type = "button" }) {
     <button
       type={type}
       onClick={onClick}
-      className="rounded-full bg-light-button-primary p-2 px-4 text-color-neutral-white focus:outline-none focus:ring-1 focus:ring-dark-border-tertiary dark:bg-dark-button-primary dark:text-dark-text-primary dark:focus:ring-dark-border-primary"
+      className="rounded-full bg-light-button-primary p-2 px-4 text-color-neutral-white hover:opacity-85 focus:outline-none focus:ring-1 focus:ring-dark-border-tertiary dark:bg-dark-button-primary dark:text-dark-text-primary dark:focus:ring-dark-border-primary"
     >
       {children}
     </button>
