ef4bb59a51f5658953c69a7a4150fc054f09495b
ttowntom <git.running054@passinbox.com>
2025-01-15 15:57:56 +0100

    enhances InputTextField component with value, placeholder, and onChange props

--- src/components/InputTextField.jsx
+++ src/components/InputTextField.jsx
@@ -1,4 +1,10 @@
-export default function InputTextField({ label, type }) {
+export default function InputTextField({
+  label,
+  type,
+  value,
+  placeholder,
+  onChange,
+}) {
   return (
     <div className="flex flex-col gap-2">
       <label htmlFor={label} className="font-medium">
@@ -7,6 +13,10 @@ export default function InputTextField({ label, type }) {
       <input
         type={type}
         id={label}
+        name={label}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
         className="rounded-md border border-light-border-secondary p-2 focus:outline-none focus:ring-1 focus:ring-light-border-primary dark:border-dark-border-primary dark:bg-dark-bg-primary dark:text-dark-text-primary dark:focus:ring-dark-border-primary"
       />
     </div>
