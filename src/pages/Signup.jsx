bec7fd21e4edf4eb946f0976beed800c29b25742
ttowntom <git.running054@passinbox.com>
2025-01-16 12:37:57 +0100

    adds signup form with validation and error handling

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,3 +1,131 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { byPrefixAndName } from "@awesome.me/kit-8d12afa6e5/icons";
+import { z } from "zod";
+import { useFormValidation } from "../hooks/useFormValidation";
+import { postFn } from "../utils/http";
+import signupSchema from "../schemas/signup";
+import InputTextField from "../components/InputTextField";
+import Button from "../components/Button";
+import Notification from "../components/Notification";
+
 export default function Signup() {
-  return <p>Signup page</p>;
+  const { formData, formErrors, handleBlur, handleChange, setFormErrors } =
+    useFormValidation(
+      {
+        name: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+      },
+      signupSchema
+    );
+  const navigate = useNavigate();
+
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: (body) => postFn({ url: "/auth/register", body }),
+    onSuccess: (data) => {
+      console.log(data);
+      // TODO: Login user and redirect to dashboard
+      // navigate("/");
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      signupSchema.parse(formData);
+      setFormErrors({});
+      await mutate({
+        name: formData.name,
+        email: formData.email,
+        password: formData.password,
+      });
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const errorObj = error.errors.reduce((acc, curr) => {
+          acc[curr.path[0]] = curr.message;
+          return acc;
+        }, {});
+        setFormErrors(errorObj);
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center text-light-text-primary dark:text-dark-text-primary">
+      <div className="space-y-4 text-center">
+        <p className="font-semibold">Join</p>
+        <h1 className="font-notoSerif text-4xl font-semibold sm:text-5xl">
+          Get started
+        </h1>
+        <p>Create your account to start booking accommodations today.</p>
+      </div>
+      <div className="mt-12 flex w-full max-w-[50ch] flex-col gap-8">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <InputTextField
+            label="Name"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={formErrors.name}
+          />
+          <InputTextField
+            label="Email"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={formErrors.email}
+          />
+          <InputTextField
+            label="Password"
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={formErrors.password}
+          />
+          <InputTextField
+            label="Confirm Password"
+            type="password"
+            name="passwordConfirm"
+            value={formData.passwordConfirm}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={formErrors.passwordConfirm}
+          />
+          <Button type="submit" onClick={handleSubmit} disabled={isPending}>
+            Sign up
+          </Button>
+        </form>
+        {isError && (
+          <div className="mt-2">
+            <Notification type="error">{error.message}</Notification>
+          </div>
+        )}
+        <div className="flex flex-wrap items-center justify-center gap-4">
+          <p>Already have a user? </p>
+          <Link
+            to="/login"
+            className="font-medium text-dark-link-primary hover:underline"
+          >
+            Log in
+            <FontAwesomeIcon
+              icon={byPrefixAndName.fas["chevron-right"]}
+              className="ml-2 text-light-text-primary dark:text-dark-text-primary"
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
