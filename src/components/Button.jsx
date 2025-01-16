bb1e20e79f0f56c21a029228f548eedd5e33bee4
ttowntom <git.running054@passinbox.com>
2025-01-16 12:37:39 +0100

    adds disabled prop to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
-export default function Button({ onClick, children, type = "button" }) {
+export default function Button({
+  onClick,
+  children,
+  type = "button",
+  disabled = false,
+}) {
   return (
     <button
       type={type}
       onClick={onClick}
+      disabled={disabled}
       className="rounded-full bg-light-button-primary p-2 px-4 text-color-neutral-white hover:opacity-85 focus:outline-none focus:ring-1 focus:ring-dark-border-tertiary dark:bg-dark-button-primary dark:text-dark-text-primary dark:focus:ring-dark-border-primary"
     >
       {children}
