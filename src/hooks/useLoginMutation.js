73c38878bd15695fa18bcb2bdf23490209c975e5
ttowntom <git.running054@passinbox.com>
2025-01-17 15:23:31 +0100

    refactor login mutation to integrate user data into global state and local storage

--- src/hooks/useLoginMutation.js
+++ src/hooks/useLoginMutation.js
@@ -2,20 +2,27 @@ import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { postFn } from "../utils/http";
 import { saveLocal } from "../utils/localStorage";
+import useUserStore from "../store/userStore";
 
 export function useLoginMutation() {
   const navigate = useNavigate();
+  const loginUser = useUserStore((state) => state.login);
+
+  let userData = {};
 
   return useMutation({
     mutationFn: (body) => postFn({ url: "/auth/login", body }),
     onSuccess: (data) => {
-      console.log(data);
-      saveLocal("user", {
+      userData = {
         token: data.data.accessToken,
         name: data.data.name,
         email: data.data.email,
+        avatar: data.data.avatar.url,
         preferredTheme: "light",
-      });
+      };
+      loginUser(userData);
+      saveLocal("user", userData);
+
       navigate("/");
     },
   });
