9de0163f57cf322533c7ddf3b6563dcaf2b88dfa
ttowntom <git.running054@passinbox.com>
2025-01-20 11:28:57 +0100

    fix: update Profile component to handle default user image when avatar is unavailable

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,12 +3,15 @@ import { useQuery } from "@tanstack/react-query";
 import useUserStore from "../store/userStore.js";
 import { fetchFn } from "../utils/http.js";
 import Notification from "../components/Notification.jsx";
+import { NO_USER_IMG_URL } from "../constants.js";
 
 export default function Profile() {
   const { name } = useParams();
   const user = useUserStore((state) => state.user);
 
   const isSelf = user.name === name;
+  const defaultImg =
+    "https://images.unsplash.com/photo-1579547945413-497e1b99dac0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&q=80&h=400&w=400";
 
   // Fetch user data
   const { data, isLoading, isError, error } = useQuery({
@@ -26,7 +29,11 @@ export default function Profile() {
         <div className="flex flex-col items-center gap-4 dark:text-dark-text-primary">
           <div className="flex items-center gap-4">
             <img
-              src={data.data.avatar.url}
+              src={
+                (data.data.avatar.url = defaultImg
+                  ? NO_USER_IMG_URL
+                  : data.data.avatar.url)
+              }
               alt={data.data.name}
               className="h-16 w-16 rounded-full"
             />
