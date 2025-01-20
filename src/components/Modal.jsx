7319fe584463b42d8aeb18d314e6a3a9a698c5aa
ttowntom <git.running054@passinbox.com>
2025-01-20 14:01:28 +0100

    refactor: update Modal component styles and remove unused icon imports

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { byPrefixAndName } from "@awesome.me/kit-8d12afa6e5/icons";
 
 export default function Modal({ children, onClose }) {
   const dialog = useRef();
@@ -26,17 +24,10 @@ export default function Modal({ children, onClose }) {
     <dialog
       ref={dialog}
       onClose={handleClose}
-      className="dark:text-dark-text-primaryrounded-lg bg-light-bg-primary p-6 text-light-text-primary backdrop:bg-black/50 dark:bg-dark-bg-primary"
+      className="min-w-[80%] max-w-[90%] rounded-lg bg-light-bg-primary p-6 text-light-text-primary backdrop:bg-black/50 dark:bg-dark-bg-primary dark:text-dark-text-primary md:min-w-[45ch]"
     >
       <div className="flex flex-col gap-6">
-        <div>
-          <button onClick={handleClose} className="absolute right-4 top-4">
-            <FontAwesomeIcon
-              icon={byPrefixAndName.fas[`circle-xmark`]}
-              className="text-color-system-accent-pink hover:text-color-system-accent-pink-dark text-3xl"
-            />
-          </button>
-        </div>
+        <div></div>
         {children}
       </div>
     </dialog>,
