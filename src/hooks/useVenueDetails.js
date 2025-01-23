2489c01fa4f3b15b882b212c8e80e9a4bc689572
ttowntom <git.running054@passinbox.com>
2025-01-23 15:17:24 +0100

    fix: update query key in useVenueDetails hook to include owner and bookings parameters

--- src/hooks/useVenueDetails.js
+++ src/hooks/useVenueDetails.js
@@ -3,7 +3,7 @@ import { fetchFn } from "../utils/http.js";
 
 export function useVenueDetails(venueId) {
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: [`/holidaze/venues/${venueId}`],
+    queryKey: [`/holidaze/venues/${venueId}?_owner=true&_bookings=true`],
     queryFn: fetchFn,
   });
 
