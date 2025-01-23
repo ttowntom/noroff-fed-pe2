7dec361b7cf5ba4c7b4d77f055222d9d1d244d01
ttowntom <git.running054@passinbox.com>
2025-01-23 15:12:47 +0100

    refactor: wrap Amenities component content in a section for better structure

--- src/components/venue/Amenities.jsx
+++ src/components/venue/Amenities.jsx
@@ -35,35 +35,42 @@ function Meta({ meta, metaKey, icon, iconType }) {
 
 export default function ({ venue }) {
   return (
-    <ul className="grid grid-cols-1 gap-1 text-light-text-primary dark:text-dark-text-primary sm:grid-cols-2">
-      <li className="flex items-center gap-2">
-        <FontAwesomeIcon
-          icon={
-            byPrefixAndName.fad[
-              venue.maxGuests > 1 ? "user-group-simple" : "user"
-            ]
-          }
-          className="text-light-border-success dark:text-dark-border-success"
+    <section>
+      <ul className="grid grid-cols-1 gap-1 text-light-text-primary dark:text-dark-text-primary sm:grid-cols-2">
+        <li className="flex items-center gap-2">
+          <FontAwesomeIcon
+            icon={
+              byPrefixAndName.fad[
+                venue.maxGuests > 1 ? "user-group-simple" : "user"
+              ]
+            }
+            className="text-light-border-success dark:text-dark-border-success"
+          />
+          <p>
+            <span className="font-semibold">Max Guests: </span>
+            {venue.maxGuests}
+          </p>
+        </li>
+        <Meta
+          meta={venue.meta.wifi}
+          metaKey="WiFi"
+          icon="wifi"
+          iconType="fas"
         />
-        <p>
-          <span className="font-semibold">Max Guests: </span>
-          {venue.maxGuests}
-        </p>
-      </li>
-      <Meta meta={venue.meta.wifi} metaKey="WiFi" icon="wifi" iconType="fas" />
-      <Meta
-        meta={venue.meta.parking}
-        metaKey="Parking"
-        icon="parking"
-        iconType="fas"
-      />
-      <Meta
-        meta={venue.meta.breakfast}
-        metaKey="Breakfast"
-        icon="egg-fried"
-        iconType="fas"
-      />
-      <Meta meta={venue.meta.pets} metaKey="Pets" icon="paw" iconType="fas" />
-    </ul>
+        <Meta
+          meta={venue.meta.parking}
+          metaKey="Parking"
+          icon="parking"
+          iconType="fas"
+        />
+        <Meta
+          meta={venue.meta.breakfast}
+          metaKey="Breakfast"
+          icon="egg-fried"
+          iconType="fas"
+        />
+        <Meta meta={venue.meta.pets} metaKey="Pets" icon="paw" iconType="fas" />
+      </ul>
+    </section>
   );
 }
