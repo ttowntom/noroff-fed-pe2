bc8b66479b27c9d398747da86312ef8436743f0c
ttowntom <git.running054@passinbox.com>
2025-01-24 05:29:52 +0100

    feat: wrap Owner component content in a Link to navigate to owner's profile

--- src/components/venue/Owner.jsx
+++ src/components/venue/Owner.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Owner({ venue }) {
   return (
     <section>
       <hr className="my-6" />
-      <div className="flex flex-col items-center gap-4 py-2 text-light-text-primary dark:text-dark-text-primary xs:flex-row">
+      <Link
+        to={`/profile/${venue.owner.name}`}
+        className="flex flex-col items-center gap-4 py-2 text-light-text-primary dark:text-dark-text-primary xs:flex-row"
+      >
         <img
           src={venue.owner.avatar.url}
           alt={venue.owner.name}
@@ -13,7 +18,7 @@ export default function Owner({ venue }) {
           <p className="font-semibold">{venue.owner.name}</p>
           <p>{venue.owner.bio}</p>
         </div>
-      </div>
+      </Link>
       <hr className="my-4" />
     </section>
   );
