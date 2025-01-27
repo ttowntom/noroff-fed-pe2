fb311808ba884de093ca956d93b7181a407d01c9
ttowntom <git.running054@passinbox.com>
2025-01-27 09:57:14 +0100

    refactor: update Owner component to use placeholder image for default avatar

--- src/components/venue/Owner.jsx
+++ src/components/venue/Owner.jsx
@@ -1,6 +1,15 @@
 import { Link } from "react-router-dom";
+import { NO_USER_IMG_URL } from "../../constants.js";
 
 export default function Owner({ venue }) {
+  let avatar = venue.owner.avatar.url;
+  if (
+    avatar ===
+    "https://images.unsplash.com/photo-1579547945413-497e1b99dac0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&q=80&h=400&w=400"
+  ) {
+    avatar = NO_USER_IMG_URL;
+  }
+
   return (
     <section>
       <hr className="my-6" />
@@ -9,7 +18,7 @@ export default function Owner({ venue }) {
         className="flex flex-col items-center gap-4 py-2 text-light-text-primary dark:text-dark-text-primary xs:flex-row"
       >
         <img
-          src={venue.owner.avatar.url}
+          src={avatar}
           alt={venue.owner.name}
           className="h-16 w-16 rounded-full"
         />
