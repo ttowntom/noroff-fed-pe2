0b07df81ec45b54e88e0167112daf043a3b3e6c4
ttowntom <git.running054@passinbox.com>
2025-01-28 10:09:17 +0100

    feat: add GalleryDesktop component for desktop image gallery display

---
+++ src/components/venue/GalleryDesktop.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { byPrefixAndName } from "@awesome.me/kit-8d12afa6e5/icons";
+import GalleryModal from "./GalleryModal";
+
+export default function GalleryDesktop({ venue }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeImageUrl, setActiveImageUrl] = useState(null);
+
+  const numImgs = venue.media.length;
+
+  function handleImageClick(imageUrl) {
+    setActiveImageUrl(imageUrl);
+    setIsOpen(true);
+  }
+
+  function handleClose() {
+    setIsOpen(false);
+    setActiveImageUrl(null);
+  }
+
+  return (
+    <div className="mb-8 hidden max-h-[600px] gap-4 md:grid md:grid-cols-2">
+      <img
+        src={venue.media[0].url}
+        alt={venue.name}
+        onClick={() => handleImageClick(venue.media[0].url)}
+        className="h-full max-h-[600px] w-full cursor-pointer rounded-md object-cover"
+      />
+      <div
+        className={`grid max-h-[600px] ${numImgs < 5 && "grid-cols-1"} ${numImgs > 4 && "grid-cols-2"} gap-4`}
+      >
+        {venue.media.slice(1, 5).map((img, idx) => (
+          <img
+            key={idx}
+            src={img.url}
+            alt={venue.name}
+            onClick={() => handleImageClick(img.url)}
+            className={`h-full ${numImgs > 2 && "max-h-[292px]"} w-full cursor-pointer rounded-md object-cover`}
+          />
+        ))}
+      </div>
+      {numImgs > 4 && (
+        <div className="col-span-2 flex w-full flex-wrap gap-4">
+          {venue.media.slice(5).map((img, idx) => (
+            <img
+              key={idx}
+              src={img.url}
+              alt={venue.name}
+              onClick={() => handleImageClick(img.url)}
+              className={`h-32 w-32 cursor-pointer rounded-md object-cover`}
+            />
+          ))}
+        </div>
+      )}
+      {isOpen && (
+        <GalleryModal
+          venue={venue}
+          onClose={() => setIsOpen(false)}
+          activeImageUrl={activeImageUrl}
+        >
+          <img src={activeImageUrl} alt={venue.name} className="object-cover" />
+        </GalleryModal>
+      )}
+    </div>
+  );
+}
