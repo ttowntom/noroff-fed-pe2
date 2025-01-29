ef5124a1dc32a6a4885cd64635e4de53b6c8382a
ttowntom <git.running054@passinbox.com>
2025-01-29 14:41:06 +0100

    feat: add LinkButton for adding new venues in VenueManager component

--- src/pages/VenueManager.jsx
+++ src/pages/VenueManager.jsx
@@ -3,6 +3,7 @@ import { byPrefixAndName } from "@awesome.me/kit-8d12afa6e5/icons";
 import useUserStore from "../store/userStore";
 import Notification from "../components/Notification.jsx";
 import NotManagerMsg from "../components/venueManager/NotManagerMsg.jsx";
+import LinkButton from "../components/LinkButton.jsx";
 
 export default function VenueManager() {
   const user = useUserStore((state) => state.user);
@@ -16,6 +17,17 @@ export default function VenueManager() {
         <p>View and update your venue listings in one place.</p>
       </div>
       {!user.venueManager && <NotManagerMsg />}
+      {user.venueManager && (
+        <div className="flex flex-col gap-4">
+          <LinkButton to="/venues/new-edit">
+            <FontAwesomeIcon
+              icon={byPrefixAndName.fas["plus"]}
+              className="mr-2 text-light-text-alternate dark:text-dark-text-primary"
+            />
+            Add new venue
+          </LinkButton>
+        </div>
+      )}
     </div>
   );
 }
