b96ebe8735591c49135b1a93639111651d7bdd91
ttowntom <git.running054@passinbox.com>
2025-01-29 14:40:04 +0100

    feat: add variant prop to Button and LinkButton components for customizable styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,13 +3,22 @@ export default function Button({
   children,
   type = "button",
   disabled = false,
+  variant = "primary",
 }) {
+  const globalStyle = "rounded-full p-2 px-4 hover:opacity-80 ";
+  const style = {
+    primary:
+      "bg-light-button-primary text-light-text-alternate focus:outline-none focus:ring-1 focus:ring-dark-border-tertiary dark:bg-dark-button-primary dark:text-dark-text-primary dark:focus:ring-dark-border-primary",
+    secondary:
+      "border border-light-link-primary bg-light-bg-primary text-light-link-primary dark:border-dark-link-primary dark:bg-dark-bg-primary dark:text-dark-link-primary hover:font-semibold",
+  };
+
   return (
     <button
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className="rounded-full bg-light-button-primary p-2 px-4 text-color-neutral-white hover:opacity-85 focus:outline-none focus:ring-1 focus:ring-dark-border-tertiary dark:bg-dark-button-primary dark:text-dark-text-primary dark:focus:ring-dark-border-primary"
+      className={`${globalStyle} ${style[variant]}`}
     >
       {children}
     </button>
