e435874068ce0a964d5787fabbd3a7b6cedc2364
ttowntom <git.running054@passinbox.com>
2025-01-29 14:40:50 +0100

    feat: add required prop to InputTextField component for enhanced validation

--- src/components/InputTextField.jsx
+++ src/components/InputTextField.jsx
@@ -9,6 +9,7 @@ export default function InputTextField({
   onChange,
   onBlur,
   error,
+  required,
 }) {
   return (
     <div className="flex flex-col gap-2">
@@ -23,6 +24,7 @@ export default function InputTextField({
         placeholder={placeholder}
         onChange={onChange}
         onBlur={onBlur}
+        required={required}
         className="rounded-md border border-light-border-secondary p-2 focus:outline-none focus:ring-1 focus:ring-light-border-primary dark:border-dark-border-primary dark:bg-dark-bg-primary dark:text-dark-text-primary dark:focus:ring-dark-border-primary"
       />
       {error && (
