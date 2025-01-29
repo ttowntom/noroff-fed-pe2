514408a79f16136065acb43f043fef87e179a563
ttowntom <git.running054@passinbox.com>
2025-01-29 09:55:30 +0100

    feat: include venueManager in login mutation response

--- src/hooks/useLoginMutation.js
+++ src/hooks/useLoginMutation.js
@@ -11,13 +11,14 @@ export function useLoginMutation() {
   let userData = {};
 
   return useMutation({
-    mutationFn: (body) => postFn({ url: "/auth/login", body }),
+    mutationFn: (body) => postFn({ url: "/auth/login?_holidaze=true", body }),
     onSuccess: (data) => {
       userData = {
         token: data.data.accessToken,
         name: data.data.name,
         email: data.data.email,
         avatar: data.data.avatar.url,
+        venueManager: data.data.venueManager,
         preferredTheme: "light",
       };
       loginUser(userData);
