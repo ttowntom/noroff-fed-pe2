f6d83f440f24b30edb1b566622247c307174ff0c
ttowntom <git.running054@passinbox.com>
2025-01-30 15:17:39 +0100

    feat: form for adding new venue

--- src/pages/NewEditVenueForm.jsx
+++ src/pages/NewEditVenueForm.jsx
@@ -1,38 +1,57 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { byPrefixAndName } from "@awesome.me/kit-8d12afa6e5/icons";
-import InputTextField from "../components/InputTextField";
-import InputTextArea from "../components/InputTextArea";
-import InputCheckRadio from "../components/InputCheckRadio";
-import SetLocation from "../components/venueManager/SetLocation";
+import { z } from "zod";
+import { useMutation } from "@tanstack/react-query";
+import { postFn } from "../utils/http";
+import { useFormValidation } from "../hooks/useFormValidation";
+import { useVenueTransform } from "../hooks/useVenueTransform";
+import useUserStore from "../store/userStore";
+import newEditVenueSchema from "../schemas/newEditVenue";
 import Button from "../components/Button";
+import Notification from "../components/Notification";
+import ImagesSection from "../components/venueManager/ImagesSection";
+import RatingSection from "../components/venueManager/RatingSection";
+import AmenitiesSection from "../components/venueManager/AmenitiesSection";
+import LocationSection from "../components/venueManager/LocationSection";
+import VenueDetailsSection from "../components/venueManager/VenueDetailsSection";
+import PriceCapacitySection from "../components/venueManager/PriceCapacitySection";
 
 export default function NewEditVenueForm() {
+  const user = useUserStore((state) => state.user);
   const navigate = useNavigate();
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
-  const [images, setImages] = useState([]);
+  const [formIsValid, setFormIsValid] = useState(undefined);
+  const { transformToApiFormat } = useVenueTransform();
 
-  const [formData, setFormData] = useState({
-    address: "",
-    city: "",
-    country: "",
-    zip: "",
-    lat: 0,
-    lng: 0,
-  });
-
-  function handleLocationSet(locationData) {
-    setFormData({
-      address: locationData.address || "",
-      city: locationData.city || "",
-      country: locationData.country || "",
-      zip: locationData.postcode || "",
-      lat: locationData.lat,
-      lng: locationData.lng,
-    });
-  }
+  const {
+    formData,
+    formErrors,
+    setFormData,
+    setFormErrors,
+    handleBlur,
+    handleChange,
+  } = useFormValidation(
+    {
+      venueName: "",
+      description: "",
+      price: "",
+      maxGuests: "",
+      rating: 0,
+      address: "",
+      city: "",
+      country: "",
+      zip: "",
+      lat: 0,
+      lng: 0,
+      images: [],
+      wifi: false,
+      parking: false,
+      breakfast: false,
+      pets: false,
+    },
+    newEditVenueSchema
+  );
 
   function handleCancel() {
     navigate(-1);
@@ -41,14 +60,57 @@ export default function NewEditVenueForm() {
   function handleImageAdd() {
     const imageUrl = document.querySelector('input[name="imageUrl"]').value;
     if (imageUrl) {
-      setImages((prev) => [...prev, imageUrl]);
+      setFormData((prev) => ({
+        ...prev,
+        images: [...prev.images, imageUrl],
+      }));
+      // Reset input
+      document.querySelector('input[name="imageUrl"]').value = "";
     }
-    // Reset the input field
-    document.querySelector('input[name="imageUrl"]').value = "";
   }
 
   function handleImageDelete(index) {
-    setImages((prevImages) => prevImages.filter((_, i) => i !== index));
+    setFormData((prev) => ({
+      ...prev,
+      images: prev.images.filter((_, i) => i !== index),
+    }));
+  }
+
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: ({ venueData, token }) =>
+      postFn({
+        url: "/holidaze/venues",
+        body: venueData,
+        token,
+      }),
+    onSuccess: (data) => {
+      navigate(`/venues/${data.data.id}`);
+    },
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      newEditVenueSchema.parse({
+        ...formData,
+        price: Number(formData.price),
+        maxGuests: Number(formData.maxGuests),
+        rating: Number(formData.rating),
+        images: formData.images,
+      });
+      const venueData = transformToApiFormat(formData);
+      setFormIsValid(true);
+      mutate({ venueData, token: user.token });
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const errorObj = error.errors.reduce((acc, curr) => {
+          acc[curr.path[0]] = curr.message;
+          return acc;
+        }, {});
+        setFormErrors(errorObj);
+        setFormIsValid(false);
+      }
+    }
   }
 
   return (
@@ -62,206 +124,58 @@ export default function NewEditVenueForm() {
         </p>
       </div>
       <form className="mx-auto mt-6 flex w-full max-w-[65ch] flex-col gap-4">
-        <section className="mb-4 flex flex-col gap-4">
-          <h2 className="text-xl font-bold sm:text-2xl">Venue details</h2>
-          <InputTextField
-            label="Venue name"
-            name="venueName"
-            type="text"
-            placeholder="Enter venue name"
-            required
-          />
-          <InputTextArea
-            label="Description"
-            name="description"
-            placeholder="Enter a description"
-            required
-          />
-        </section>
-        <section className="mb-4 flex flex-col gap-4">
-          <h2 className="mt-4 text-xl font-bold sm:text-2xl">Location</h2>
-          <p className="-mb-2 text-sm">
-            Click on the map to set the location of the venue.
-          </p>
-          <SetLocation onLocationSet={handleLocationSet} />
-          <div className="flex flex-col gap-4 sm:flex-row">
-            <div className="flex-grow">
-              <InputTextField
-                label="Address"
-                name="address"
-                type="text"
-                placeholder="Enter address"
-                value={formData.address}
-                defaultValue={formData.address}
-                onChange={(e) =>
-                  setFormData((prev) => ({ ...prev, address: e.target.value }))
-                }
-              />
-            </div>
-            <InputTextField
-              label="Country"
-              name="country"
-              type="text"
-              placeholder="Enter country"
-              value={formData.country}
-              defaultValue={formData.country}
-              onChange={(e) =>
-                setFormData((prev) => ({ ...prev, country: e.target.value }))
-              }
-            />
-          </div>
-          <div className="flex flex-col gap-4 sm:flex-row">
-            <InputTextField
-              label="Zip code"
-              name="zip"
-              type="text"
-              placeholder="Enter zip code"
-              value={formData.zip}
-              defaultValue={formData.zip}
-              onChange={(e) =>
-                setFormData((prev) => ({ ...prev, zip: e.target.value }))
-              }
-            />
-            <div className="flex-grow">
-              <InputTextField
-                label="City"
-                name="city"
-                type="text"
-                placeholder="Enter city"
-                value={formData.city}
-                defaultValue={formData.city}
-                onChange={(e) =>
-                  setFormData((prev) => ({ ...prev, city: e.target.value }))
-                }
-              />
-            </div>
-          </div>
-        </section>
-        <section className="mb-4 flex flex-col gap-4">
-          <h2 className="mt-4 text-xl font-bold sm:text-2xl">
-            Price and capacity
-          </h2>
-          <div className="flex flex-col gap-4 sm:flex-row">
-            <InputTextField
-              label="Price per night"
-              name="price"
-              type="number"
-              placeholder="Enter price"
-            />
-            <InputTextField
-              label="Max guests"
-              name="maxGuests"
-              type="number"
-              placeholder="Enter max guests"
-            />
-          </div>
-        </section>
-        <section className="mb-4 flex flex-col gap-4">
-          <h2 className="mt-4 text-xl font-bold sm:text-2xl">Amenities</h2>
-          <div className="grid max-w-fit grid-cols-1 gap-4 xs:grid-cols-2">
-            <InputCheckRadio
-              label="Wi-Fi"
-              name="wifi"
-              type="checkbox"
-              value="wifi"
-            />
-            <InputCheckRadio
-              label="Parking"
-              name="parking"
-              type="checkbox"
-              value="parking"
-            />
-            <InputCheckRadio
-              label="Breakfast"
-              name="breakfast"
-              type="checkbox"
-              value="breakfast"
-            />
-            <InputCheckRadio
-              label="Pets allowed"
-              name="pets"
-              type="checkbox"
-              value="pets"
-            />
-          </div>
-        </section>
-        <section className="mb-4 flex flex-col gap-4">
-          <h2 className="mt-4 text-xl font-bold sm:text-2xl">Rating</h2>
-          <div className="flex items-center gap-1">
-            {rating > 0 && (
-              <button onClick={() => setRating(0)} className="text-xl">
-                <FontAwesomeIcon
-                  icon={byPrefixAndName.fas["star-exclamation"]}
-                  className="mr-2 text-xl text-light-text-error dark:text-dark-text-error"
-                />
-              </button>
-            )}
-            {[1, 2, 3, 4, 5].map((star) => (
-              <button
-                type="button"
-                key={star}
-                onClick={() => setRating(star)}
-                onMouseEnter={() => setHover(star)}
-                onMouseLeave={() => setHover(0)}
-                className="text-xl"
-              >
-                <FontAwesomeIcon
-                  icon={
-                    star <= (hover || rating)
-                      ? byPrefixAndName.fas["star"]
-                      : byPrefixAndName.far["star"]
-                  }
-                  className="text-light-text-warning dark:text-dark-text-warning"
-                />
-              </button>
-            ))}
-            <p>({rating} stars)</p>
-          </div>
-        </section>
-        <section className="mb-4 flex flex-col gap-4">
-          <h2 className="mt-4 text-xl font-bold sm:text-2xl">Images</h2>
-          <div className="grid grid-cols-1 gap-4 xs:grid-cols-2">
-            {images.map((image, index) => (
-              <div key={index} className="relative">
-                <img
-                  src={image}
-                  alt={`Image ${index + 1}`}
-                  className="h-40 w-full rounded-md object-cover"
-                />
-                <button
-                  type="button"
-                  onClick={() => handleImageDelete(index)}
-                  className="absolute -right-2 -top-2 flex items-center justify-center rounded-full bg-color-neutral-white hover:opacity-80"
-                >
-                  <FontAwesomeIcon
-                    icon={byPrefixAndName.fas["circle-xmark"]}
-                    className="text-xl text-light-text-error dark:text-dark-text-error"
-                  />
-                </button>
-              </div>
-            ))}
-          </div>
-          <div className="flex flex-wrap items-end gap-4">
-            <div class="flex-grow">
-              <InputTextField
-                label="Image URL"
-                name="imageUrl"
-                type="url"
-                placeholder="Enter image URL"
-              />
-            </div>
-            <button
-              type="button"
-              onClick={handleImageAdd}
-              className="rounded-md bg-light-button-primary px-4 py-2 text-light-text-alternate hover:opacity-80 dark:bg-dark-button-primary dark:text-dark-text-primary"
-            >
-              + Add image
-            </button>
-          </div>
-        </section>
+        <VenueDetailsSection
+          formErrors={formErrors}
+          handleBlur={handleBlur}
+          handleChange={handleChange}
+        />
+        <LocationSection
+          formErrors={formErrors}
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+        />
+        <PriceCapacitySection
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+          formErrors={formErrors}
+        />
+
+        <AmenitiesSection
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+          formErrors={formErrors}
+        />
+
+        <RatingSection
+          onRatingChange={(value) =>
+            setFormData((prev) => ({ ...prev, rating: value }))
+          }
+        />
+        <ImagesSection
+          images={formData.images}
+          onImageAdd={handleImageAdd}
+          onImageDelete={handleImageDelete}
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+          formErrors={formErrors}
+        />
+
         <section className="mt-12 flex flex-col gap-4">
-          <Button type="submit">Add venue</Button>
-          <Button type="button" variant="secondary" onClick={handleCancel}>
+          {formIsValid === false && (
+            <Notification type="error">
+              Please fix the errors in the form before submitting.
+            </Notification>
+          )}
+          {isError && <Notification type="error">{error.message}</Notification>}
+          <Button onClick={handleSubmit} type="submit">
+            Add venue
+          </Button>
+          <Button
+            type="button"
+            disabled={isPending}
+            variant="secondary"
+            onClick={handleCancel}
+          >
             Cancel
           </Button>
         </section>
