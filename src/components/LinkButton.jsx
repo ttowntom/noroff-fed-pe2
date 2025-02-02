56486706cc1bdf37ab3f36e7db116888bb7786b1
ttowntom <git.running054@passinbox.com>
2025-02-02 12:55:12 +0100

    fix: add text-center alignment to LinkButton for improved layout

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 export default function LinkButton({ to, children, variant = "primary" }) {
-  const globalStyle = "rounded-full p-2 px-4 hover:opacity-80 ";
+  const globalStyle = "rounded-full p-2 px-4 hover:opacity-80 text-center";
   const style = {
     primary:
       "bg-light-button-primary text-light-text-alternate focus:outline-none focus:ring-1 focus:ring-dark-border-tertiary dark:bg-dark-button-primary dark:text-dark-text-primary dark:focus:ring-dark-border-primary",
