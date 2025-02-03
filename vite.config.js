7e47ae3bd9ea995583f664d37b51ec6de58babe2
ttowntom <git.running054@passinbox.com>
2025-02-03 11:05:44 +0100

    fix: update script path in index.html and set base URL in vite.config.js for correct deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
-  base: "/",
+  base: "/noroff-fed-pe2/",
   plugins: [react()],
   css: {
     postcss: {
