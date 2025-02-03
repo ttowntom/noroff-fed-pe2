37071f453175cf39a4fad37b6fb236a3deeaed74
ttowntom <git.running054@passinbox.com>
2025-02-03 11:47:56 +0100

    fix: adjust overflow property in VenueDetails for improved layout and readability

--- src/pages/VenueDetails.jsx
+++ src/pages/VenueDetails.jsx
@@ -26,9 +26,9 @@ export default function VenueDetails() {
           {isLoading && <p>Loading...</p>}
 
           <Gallery venue={venue} />
-          <div className="flex flex-col gap-4 overflow-hidden md:flex-row md:justify-between md:gap-12 lg:gap-24">
+          <div className="flex flex-col gap-4 overflow-visible md:flex-row md:justify-between md:gap-12 lg:gap-24">
             <div className="w-full lg:w-2/3">
-              <h1 className="mb-4 font-notoSerif text-4xl font-semibold md:text-5xl">
+              <h1 className="word-break: mb-4 break-all font-notoSerif text-4xl font-semibold md:text-5xl">
                 {venue.name}
               </h1>
               <p>
