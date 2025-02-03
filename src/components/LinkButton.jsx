f0540e5c59800a751d49a706afd0cc8b8437190e
ttowntom <git.running054@passinbox.com>
2025-02-03 10:11:12 +0100

    fix: update LinkButton styles to include flex and items-center for better alignment

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 
 export default function LinkButton({ to, children, variant = "primary" }) {
-  const globalStyle = "rounded-full p-2 px-4 hover:opacity-80 text-center";
+  const globalStyle =
+    "rounded-full p-2 px-4 hover:opacity-80 text-center items-center flex";
   const style = {
     primary:
       "bg-light-button-primary text-light-text-alternate focus:outline-none focus:ring-1 focus:ring-dark-border-tertiary dark:bg-dark-button-primary dark:text-dark-text-primary dark:focus:ring-dark-border-primary",
