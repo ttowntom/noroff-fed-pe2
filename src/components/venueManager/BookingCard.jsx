f4330ef48da25cf7b85eb0541b23d4ae173593de
ttowntom <git.running054@passinbox.com>
2025-02-04 09:51:48 +0100

    feat: add link to customer profile in BookingCard component

--- src/components/venueManager/BookingCard.jsx
+++ src/components/venueManager/BookingCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { byPrefixAndName } from "@awesome.me/kit-8d12afa6e5/icons";
 import DateBox from "../DateBox";
@@ -9,7 +10,12 @@ export default function BookingCard({ booking }) {
     <article
       className={`flex flex-grow flex-col items-center justify-center gap-2 rounded-lg border bg-light-bg-secondary p-2 shadow-md dark:bg-dark-bg-secondary sm:px-6 sm:py-4 md:max-w-[30vw] ${isPastBooking ? "border-light-border-error" : "border-light-border-primary dark:border-dark-border-primary"}`}
     >
-      <h4 className="-mb-2 text-lg font-bold">{booking.customer.name}</h4>
+      <Link
+        to={`/profile/${booking.customer.name}`}
+        className="hover:underline"
+      >
+        <h4 className="-mb-2 text-lg font-bold">{booking.customer.name}</h4>
+      </Link>
       <p>{booking.customer.email}</p>
       <div className="flex flex-col items-center justify-center gap-2 xs:flex-row">
         <DateBox date={booking.dateFrom} />
